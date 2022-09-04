9f80d0178e414295bc5bbdcf0688a82686414850
Vincent Dsouza <vdsx03@gmail.com>
2022-09-04 21:28:51 +0530

    Added the working features

--- OOPSLab/p1.c
+++ OOPSLab/p1.c
@@ -9,33 +9,41 @@ struct Book{
 typedef struct Book Book;
 
 void insertBook(Book * a,int * nbooks){
+    char c;
     if (*nbooks != 0){
-        printf("Trigger\n");
+        //printf("Trigger\n");
         a++;//next empty position
+        c = getchar();
     }
     char lname[30],lauthor[30],lsub[25];
     lname[0] = 0;
     int lcopies;
-
     printf("Enter book name:");
-    getchar();
-    fgets(lname, 50, stdin);
+    fgets(lname, 25, stdin);
+    lname[strlen(lname)-1] = '\0';
+    // gets(lname); //this takes proper name but error for array
+    // getchar();
     strcpy(a->name, lname);
     if (lname[0]==0)
         printf("\nError\n");
     printf("Enter author name:");
-
     fgets(lauthor,50,stdin);
+    lauthor[strlen(lauthor)-1] = '\0';
+    // gets(lauthor);
+    // getchar();
     strcpy(a->author, lauthor);
     printf("Enter subject:");
     fgets(lsub,50,stdin);
+    lsub[strlen(lsub)-1] = '\0';
+    // gets(lsub);
+    //getchar();
     strcpy(a->subject, lsub);
     printf("Enter number of copies available:");
     scanf("%d",&lcopies);
     a->ncopies = lcopies;
     *nbooks = *nbooks + 1;
     if (a->name[0] == '\n')printf("Problem");
-    //fflush(stdin);
+    // fflush(stdin);
 }
 
 void displayBook(Book * a){
@@ -47,12 +55,13 @@ void displayBook(Book * a){
     printf("---------------------\n");
 }
 void displayAllBooks(Book * a,int nbooks){
+    printf("All library books:\n");
     for (int i=0;i< (nbooks);i++){
         displayBook(a);
         a++;
     }
 }
-void nameSearch  (Book * a,char * name, int nbooks){
+void nameSearch(Book * a,char * name, int nbooks){
     int q = 1;
     for (int i=0;i<(nbooks) && q;i++){
         //locate the book
@@ -60,29 +69,58 @@ void nameSearch  (Book * a,char * name, int nbooks){
             printf("\n\tLocated Book\n");
             q = 0;
             displayBook(a);
+        }else{
+            printf("Name=%sSearchName=%s\nL1=%d\tl2=%d\n",a->name,name,strlen(a->name),strlen(name));
         }
         a++;
     }
+    if (q==1)
+        printf("Book not found\n");
 }
 void authorSearch(Book * a, char * author, int nbooks){
     int q = 1;
+    //printf("nbooks=%d\n",nbooks);
     for (int i=0;i<(nbooks) && q;i++){
         //locate the book
+        //printf("Author = %s",a->author);
         if (strcmp(a->author, author)==0){
-            printf("\n\tLocated Book\n");
+            printf("\n---Located Book\n");
             q = 0;
             displayBook(a);
+        }else{
+            //printf("Author=%s, SearchAuthor=%s\n",a->author,author);
         }
         a++;
     }
+    if (q==1){
+        printf("Book not found\n");
+    }
+        
 }
-void bookDelete  (Book * a, int index = -1){
-    int i;
-    if (index==(-1)){
+void bookDelete(Book * q, int index, int * nbooks){
+    int i = index,x=*nbooks,j=0;
+    Book * a = q,*b;
+    if (1){
         printf("Enter index to delete:");
         scanf("%d",&i);
-        
     }
+    if (i==(x-1))
+        *nbooks =i;
+    //to push a to the element we wish to delete
+    while (j<i && (i!=(*nbooks))){
+        a++;
+        j++;
+    }
+    while (i<(x-1)){
+        b = a+1;
+        strcpy(a->author,b->author);
+        strcpy(a->name,b->name);
+        strcpy(a->subject,b->subject); 
+        a->ncopies = b->ncopies;
+        i++;
+        a++;
+    }
+    *nbooks = *nbooks-1;
 }
 int main(){
     int i=0,n=0; //n is for number of books in the library(unique only)
@@ -92,11 +130,16 @@ int main(){
     //insertBook(&b1,&n);
     //displayBook(&b1);
     Book * lb = library;
-    //insertBook(lb, &n);
-    //printf("One done\n");
-    //insertBook(lb, &n);
-    //displayAllBooks(library,n);
-
+    insertBook(lb, &n);
+    insertBook(lb, &n);
+    insertBook(lb, &n);
+    // insertBook(lb, &n);
+    // insertBook(lb, &n);
+    //nameSearch(lb,"vincent",n);
+    authorSearch(lb,"dsouza",n);
+    bookDelete(library,1,&n);
+    displayAllBooks(library,n);
+    /*
     do {
         printf("Enter\n 1 to insert book record\n 2 to Search based on book name\n ");
         printf("3 to Search based on author name\n 4 to Delete a book from the record\n ");
@@ -107,13 +150,13 @@ int main(){
                     insertBook(lb, &n);
                     break;
             case 2: //search by book name
-                    nameSearch(lb,n);
+                    nameSearch(lb,"vincent",n);
                     break;
             case 3: //search by book author
-                    authorSearch(lb,n);
+                    authorSearch(lb,"dsouza",n);
                     break;
             case 4: //delete a book
-                    bookDelete(lb,n);
+                    bookDelete(lb,2,&n);
                     break;
             case 5: //dislayAllbooks
                     displayAllBooks(library,n);
@@ -122,7 +165,7 @@ int main(){
             default: printf("Choose an option from (1-6):\n");
         }
     }while (i!=6);
-
+    */
     return 0;
 
 }
