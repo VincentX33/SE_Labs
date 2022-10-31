49a66f0a96e3a687537ed0eb048770050a1f48ca
Vincent Dsouza <vdsx03@gmail.com>
2022-10-31 21:16:22 +0530

    Added lab session 3 for OOPD lab

--- OOPS/Experiments/Lab3/CopyConst.cpp
+++ OOPS/Experiments/Lab3/CopyConst.cpp
@@ -1,41 +1,39 @@
 #include <iostream>
-
 using namespace std;
 //copy constructor
 class Sample{
-    int x;
-    string d;
-    public:
-        Sample();
-        Sample(Sample &);
-        void display();
-        void input();
+  int x;
+  string d;
+  public:
+    Sample();
+    Sample(Sample &);
+    void display();
+    void input();
 };
 Sample::Sample(){
-    x = 0;
-    d = "";
+  x = 0;
+  d = "";
 }
 Sample::Sample(Sample & U){
-    x = U.x;
-    d = U.d;
+  x = U.x;
+  d = U.d;
 }
 void Sample::input(){
-    cout<<"Enter x:";
-    cin>>x;
-    cin.ignore();
-    cout<<"Enter textstring:";
-    getline(cin,d);
+  cout<<"Enter x:";
+  cin>>x;
+  cin.ignore();
+  cout<<"Enter textstring:";
+  getline(cin,d);
 }
 void Sample::display(){
-    cout<<"X   :"<<x<<"\nText:"<<d<<"\n";
+  cout<<"X   :"<<x<<"\nText:"<<d<<"\n";
 }
-
 int main(){
-    Sample X;
-    X.input();
-    cout<<"Copying contents into another object...\n";
-    Sample Y(X);
-    cout<<"Displaying second object\n";
-    Y.display();
-    return 0;
+  Sample X;
+  X.input();
+  cout<<"Copying contents into another object...\n";
+  Sample Y(X);
+  cout<<"Displaying second object\n";
+  Y.display();
+  return 0;
 }
--- OOPS/Experiments/Lab3/paramConstr.cpp
+++ OOPS/Experiments/Lab3/paramConstr.cpp
@@ -1,47 +1,46 @@
 #include <bits/stdc++.h>
 //parameterized constructors
-
 using namespace std;
 class Student{
-    string name;
-    int age;
-    float cgpa;
-    public :    
-        Student();
-        Student(string,int,float);
-        void display();
-        void getinfo();
+  string name;
+  int age;
+  float cgpa;
+  public :    
+    Student();
+    Student(string,int,float);
+    void display();
+    void getinfo();
 };
 Student::Student(){
-    name = "";
-    age = 0;
-    cgpa = 0;
+  name = "";
+  age = 0;
+  cgpa = 0;
 }
 Student::Student(string n,int a = 0,float f = 0){
-    name = n;
-    age = a;
-    cgpa = f;
+  name = n;
+  age = a;
+  cgpa = f;
 }
 void Student::display(){
-    cout<<"Student Details\n";
-    cout<<"Name : "<<name<<"\n";
-    cout<<"Age : "<<age<<"\n";
-    cout<<"cgpa : "<<cgpa<<"\n";
+  cout<<"Student Details\n";
+  cout<<"Name : "<<name<<"\n";
+  cout<<"Age : "<<age<<"\n";
+  cout<<"cgpa : "<<cgpa<<"\n";
 }
 void Student::getinfo(){
-    cout<<"Enter Student Details\n";
-    cout<<"Name : "<<"\n";
-    getline(cin, name);
-    cout<<"Age : "<<"\n";
-    cin>>age;
-    cout<<"cgpa : "<<"\n";
-    cin>>cgpa;
+  cout<<"Enter Student Details\n";
+  cout<<"Name : "<<"\n";
+  getline(cin, name);
+  cout<<"Age : "<<"\n";
+  cin>>age;
+  cout<<"cgpa : "<<"\n";
+  cin>>cgpa;
 }
 int main(){
-    Student X;
-    X.getinfo();
-    Student Y("Joston Pereira", 19, 25);
-    X.display();
-    Y.display();
-    return 0;
+  Student X;
+  X.getinfo();
+  Student Y("Joston Pereira", 19, 25);
+  X.display();
+  Y.display();
+  return 0;
 }
